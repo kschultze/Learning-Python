365b550d32e4686bd0e07ab267e09303d83660e6
Kevin Schultze <kschultze@gmail.com>
2016-08-11 23:13:05 -0400

    add function to match find substrings with one substitution

---
+++ Assignment_3/sub1Match.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 22:10:52 2016
+
+@author: Schultze
+"""
+
+def MatchPairOneSub(target, key):
+    """
+    returns list of instances of key string inside of target string
+    includes exact matches and matches that are off by 1 char
+    returns None if no matches
+    """
+    
+    indexList = []
+    for i in range(0,len(target)-len(key)+1):
+        
+        charMatch = 0
+        for j in range(0,len(key)):
+            if target[i + j] == key[j]:
+                charMatch += 1
+        if charMatch == len(key)-1:  #change to:  charMatch >= len(key)- 1 to include exact matches
+            indexList += [i]
+        charMatch = 0
+        
+    if indexList == []:
+        return None
+    return indexList
